5fc69e4c7c5430fee3a8b25ddac3c41664d949fd
Agustin Boccio <aboccio@hotmail.com>
2022-09-08 15:41:25 +0200

    Add home view title and description. Add Stock caution accordion item

--- src/pages/home/components/Landing.jsx
+++ src/pages/home/components/Landing.jsx
@@ -6,6 +6,16 @@ export default function Landing(){
 
     return (
         <div className='container mt-3'>
+            <section>
+                <h1>{t("appTitle")}</h1>
+                <h3>{t("appSubtitle")}</h3>
+                <article className='mt-3'>
+                    <h2>{t("How it works")}</h2>
+                    <p>{t("HIW-1")}</p>
+                    <p>{t("HIW-2")}</p>
+                    <p>{t("HIW-3")}</p>
+                </article>
+            </section>
             <div className="accordion" id="accordionSynthetic">
                 <div className="accordion-item">
                     <h2 className="accordion-header" id="headingOne">
@@ -24,6 +34,7 @@ export default function Landing(){
                         </div>
                     </div>
                 </div>
+
                 <div className="accordion-item">
                     <h2 className="accordion-header" id="headingTwo">
                     <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
@@ -41,13 +52,34 @@ export default function Landing(){
                         </div>
                     </div>
                 </div>
+
                 <div className="accordion-item">
                     <h2 className="accordion-header" id="headingThree">
                     <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                        More calculators...
+                        {t("Stock Caution")}
                     </button>
                     </h2>
                     <div id="collapseThree" className="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+                    <div className="accordion-body">
+                            <p className="card-text">{t("Caution-description-1")}</p>
+                            <p className="card-text">{t("Caution-description-2")}</p>
+                            <p className="card-text">{t("Caution-description-3")}</p>
+                            <p className="card-text">{t("Caution-description-4")}</p>
+                            <p className="card-text">{t("Caution-description-5")}</p>
+                        </div>
+                        <div className='mt-3 mb-3 text-center'>
+                            <NavLink to={"/calculate-stock-caution"} className="btn btn-primary">{t("calc-btn")} {t("Stock Caution")}</NavLink>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="accordion-item">
+                    <h2 className="accordion-header" id="headingN">
+                    <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseN" aria-expanded="false" aria-controls="collapseN">
+                        More calculators...
+                    </button>
+                    </h2>
+                    <div id="collapseN" className="accordion-collapse collapse" aria-labelledby="headingN" data-bs-parent="#accordionExample">
                     <div className="accordion-body">
                         <p><strong>Upcoming next...</strong></p>
                         <p>New features are being developed to make your financial calculations easier!</p>
