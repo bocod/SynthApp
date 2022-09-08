8e2871e79b82fb664d4b1d632428c43318755008
Agustin Boccio <aboccio@hotmail.com>
2022-09-08 14:11:08 +0200

    Restyling for better understanding

--- src/pages/caution/components/calc.jsx
+++ src/pages/caution/components/calc.jsx
@@ -22,7 +22,7 @@ export default function Calc(){
     const grossProfit = (cautionCapital * (cautionRate/100) * (cautionTerm/365)).toFixed(2);
     const exchangeFee = (((exchangeCommission/100)/30* cautionTerm) * cautionCapital).toFixed(2);
     const IVATax = (exchangeFee * (IVA/100)).toFixed(2);
-    const marketFee = (cautionCapital * (mFee/100)).toFixed(2);
+    const marketFee = (((Number(mFee)/100)/90 * cautionCapital * cautionTerm)*1.21).toFixed(2);
     
     const cautionCost = (Number(exchangeFee) + Number(IVATax) + Number(marketFee)).toFixed(2);
     const netProfit = (Number(grossProfit) - Number(cautionCost)).toFixed(2);
@@ -53,84 +53,118 @@ export default function Calc(){
             <form className="container mt-3" style={{"maxWidth": "500px"}} id="printable">
 
                 {/* Loan stage user input */}
+                <h2 className='text-center'>{t("Stock Caution")}</h2>
 
-                <h2>{t("Stock Caution")}</h2>
+                <section>
+                    <div>
+                        <h4>{t("Capital to caution")}</h4>
+                        <div className="input-group mb-3">
+                            <span className="input-group-text">$</span>
+                            {capitalInput}
+                        </div>
+                    </div>
+                    <div>
+                        <h4>{t("Rate")}</h4>
+                        <div className="input-group mb-3">
+                            {rateInput}
+                            <span className="input-group-text">%</span>
+                        </div>
+                    </div>
+                    <div>
+                        <h4>{t("Due date")}</h4>
+                        <div className="input-group mb-3">
+                            {dueDateInput}
+                        </div>
+                    </div>
 
-                <div className="input-group mb-3">
-                    <span className="input-group-text">{t("Capital to caution")}</span>
-                    <span className="input-group-text">$</span>
-                    {capitalInput}
-                </div>
-                <div className="input-group mb-3">
-                    <span className="input-group-text">{t("Rate")}</span>
-                    {rateInput}
-                    <span className="input-group-text">%</span>
-                </div>
-                <div className="input-group mb-3">
-                    <span className="input-group-text">{t("Due date")}</span>
-                    {dueDateInput}
-                </div>
+                    {/* Auto calculate */}
+                    
+                    <div className="input-group mb-3">
+                        <span className="input-group-text">{t("Term")}</span>
+                        <input value={!isNaN(cautionTerm) ? cautionTerm : ""} type="number" className="form-control" aria-label="Loan interest" disabled />
+                        <span className="input-group-text">{t("days")}</span>
+                    </div>
 
-                {/* Auto calculate */}
-                
-                <div className="input-group mb-3">
-                    <span className="input-group-text">{t("Term")}</span>
-                    <input value={!isNaN(cautionTerm) ? cautionTerm : ""} type="number" className="form-control" aria-label="Loan interest" disabled />
-                    <span className="input-group-text">{t("days")}</span>
-                </div>
-                <div className="input-group mb-3">
-                    <span className="input-group-text">{t("Gross profit")}</span>
-                    <span className="input-group-text">$</span>
-                    <input value={!isNaN(grossProfit) ? grossProfit : ""} type="number" className="form-control" aria-label="Loan interest" disabled />
-                </div>
+                    <div>
+                        <h4>{t("Gross profit")}</h4>
+                        <div className="input-group mb-3">
+                            <span className="input-group-text">$</span>
+                            <input value={!isNaN(grossProfit) ? grossProfit : ""} type="number" className="form-control" aria-label="Loan interest" disabled />
+                        </div>
+                    </div>
+                </section>
 
-                <h2>{t("Commissions, fees and taxes")}</h2>
+                <section>
+                    <h3 className='text-center'>{t("Commissions, fees and taxes")}</h3>
 
-                <div className="input-group mb-3">
-                    <span className="input-group-text">{t("Commission")}</span>
-                    {commissionInput}<span className="input-group-text">%</span>
-                    <span className="input-group-text">$</span>
-                    <input value={!isNaN(exchangeFee) ? exchangeFee : ""} type="number" className="form-control" aria-label="Total amount of commission" disabled />
-                </div>
+                    <div>
+                        <h4>{t("Commission")}</h4>
+                        <div className="input-group mb-3">
+                            {commissionInput}
+                            <span className="input-group-text">%</span>
+                        </div>
+                        <div className="input-group mb-3">
+                            <span className="input-group-text">$</span>
+                            <input value={!isNaN(exchangeFee) ? exchangeFee : ""} type="number" className="form-control" aria-label="Total amount of commission" disabled />
+                        </div>
+                    </div>
 
-                <div className="input-group mb-3">
-                    <span className="input-group-text">{t("IVA")}</span>
-                    {IVAInput}<span className="input-group-text">%</span>
-                    <span className="input-group-text">$</span>
-                    <input value={!isNaN(IVATax) ? IVATax : ""} type="number" className="form-control" aria-label="Total amount of commission" disabled />
-                </div>
+                    <div>
+                        <h4>{t("IVA")} / {t("Commission")}</h4>
+                        <div className="input-group mb-3">
+                            {IVAInput}
+                            <span className="input-group-text">%</span>
+                        </div>
+                        <div className="input-group mb-3">
+                            <span className="input-group-text">$</span>
+                            <input value={!isNaN(IVATax) ? IVATax : ""} type="number" className="form-control" aria-label="Total amount of commission" disabled />
+                        </div>
+                    </div>
 
-                <div className="input-group mb-3">
-                    <span className="input-group-text">{t("Market fee")}</span>
-                    {mFeeInput}<span className="input-group-text">%</span>
-                    <span className="input-group-text">$</span>
-                    <input value={!isNaN(marketFee) ? marketFee : ""} type="number" className="form-control" aria-label="Total amount of commission" disabled />
-                </div>
+                    <div>
+                        <h4>{t("Market fee")}</h4>
+                        <div className="input-group mb-3">
+                            {mFeeInput}
+                            <span className="input-group-text">%</span>
+                        </div>
+                        <div className="input-group mb-3">
+                            <span className="input-group-text">$</span>
+                            <input value={!isNaN(marketFee) ? marketFee : ""} type="number" className="form-control" aria-label="Total amount of commission" disabled />
+                        </div>
+                    </div>
+                </section>
 
                 {/* Subtotals */}
+                <section>
+                    <div>
+                        <h3>{t("Caution cost")}</h3>
+                        <div className="input-group mb-3">
+                            <span className="input-group-text">$</span>
+                            <input value={!isNaN(cautionCost) ? cautionCost : ""} type="number" className="form-control" aria-label="Caution cost" disabled />
+                        </div>
+                    </div>
 
-                <div className="input-group mb-3">
-                    <span className="input-group-text">{t("Caution cost")}</span>
-                    <span className="input-group-text">$</span>
-                    <input value={!isNaN(cautionCost) ? cautionCost : ""} type="number" className="form-control" aria-label="Loan gross cost" disabled />
-                </div>
+                    <div>
+                        <h3>{t("Net profit")}</h3>
+                        <div className="input-group mb-3">
+                            <span className="input-group-text">$</span>
+                            <input value={!isNaN(netProfit) ? netProfit : ""} type="number" className="form-control" aria-label="Net profit to earn" disabled />
+                        </div>
+                    </div>
 
-                <div className="input-group mb-3">
-                    <span className="input-group-text">{t("Net profit")}</span>
-                    <span className="input-group-text">$</span>
-                    <input value={!isNaN(netProfit) ? netProfit : ""} type="number" className="form-control" aria-label="Loan Subtotal to get" disabled />
-                </div>
+                    <div>
+                        <h3>{t("To be credited")}</h3>
+                        <div className="input-group mb-3">
+                            <span className={Number(credit) > Number(cautionCapital) ? "input-group-text text-success fw-bold" : "input-group-text"}>$</span>
+                            <input value={!isNaN(credit) ? credit : ""} type="number" className={Number(credit) > Number(cautionCapital) ? "form-control text-success fw-bold" : "form-control"} aria-label="Total amount to recieve at due date" disabled />
+                        </div>
+                    </div>
 
-                <div className="input-group mb-3">
-                    <span className="input-group-text">{t("To be credited")}</span>
-                    <span className="input-group-text">$</span>
-                    <input value={!isNaN(credit) ? credit : ""} type="number" className="form-control" aria-label="Loan interest" disabled />
-                </div>
-
-                <div className="mb-3">
-                    <button type="button" className="btn btn-secondary" onClick={printHandler}><i className="bi bi-printer"></i> {t("Print")}</button>
-                    <i className="bi bi-tree text-black-50 fw-light ms-3"> {t("print-disclosure")}</i>
-                </div>
+                    <div className="mb-3">
+                        <button type="button" className="btn btn-secondary" onClick={printHandler}><i className="bi bi-printer"></i> {t("Print")}</button>
+                        <i className="bi bi-tree text-black-50 fw-light ms-3"> {t("print-disclosure")}</i>
+                    </div>
+                </section>
 
                 {/* ABBR */}
 
