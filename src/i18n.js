16dee6f3e48e16aa031fbf16bb1038d54b1ea593
Agustin Boccio <aboccio@hotmail.com>
2022-09-09 21:23:15 +0200

    Incorporate catalan to dictionaries

--- src/i18n.js
+++ src/i18n.js
@@ -2,6 +2,7 @@ import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import en from "./locales/en.json";
 import es from "./locales/es.json";
+import cat from "./locales/cat.json";
 
 i18n.use(initReactI18next).init({
   fallbackLng: "en",
@@ -13,11 +14,14 @@ i18n.use(initReactI18next).init({
     es: {
       translations: es,
     },
+    cat: {
+      translations: cat,
+    },
   },
   ns: ["translations"],
   defaultNS: "translations",
 });
 
-i18n.languages = ["en", "es"];
+i18n.languages = ["en", "es", "cat"];
 
 export default i18n;
